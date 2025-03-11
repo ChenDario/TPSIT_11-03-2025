9c97aede415492ef0ca81f618cc3571086d49017
lai2-24 <lai2-24@itismeucci.com>
2025-03-11 14:35:19 +0100

    first commit

--- php/index.php
+++ php/index.php
@@ -7,5 +7,7 @@ require __DIR__ . '/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->get('/alunni', "AlunniController:index");
+$app->get('/alunni/{id}', "AlunniController:g");
+$app->post('/alunni/{id}', "AlunniController:p")
 
 $app->run();
